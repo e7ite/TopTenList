1eabae89c08ee5cc3a65704beaa1aa7c158507bd
E7ite <z7z.e7ite.z7z@gmail.com>
2020-03-30 11:46:22 -0700

    Added an interface to the functions

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -10,6 +10,18 @@ TopTenList::TopTenList()
     _list.resize(10);
 }
 
+void TopTenList::display_forward()
+{
+    for (const auto& i : this->_list)
+        std::cout << i.text << " " << i.url << std::endl;
+}
+
+void TopTenList::display_backward()
+{
+    for (auto i = this->_list.rbegin(); i != this->_list.rend(); i++)
+        std::cout << i->text << " " << i->url << std::endl;
+}
+
 void TopTenList::set_at(int index, Hyperlink link)
 {
     this->_list.at(index - 1) = link;
--- TopTenList.h
+++ TopTenList.h
@@ -1,19 +1,27 @@
 //Manages a list of top ten hyperlinks
 #ifndef TOPTENLIST_H
 #define TOPTENLIST_H
+#include <iostream>
 #include <string>
 #include <vector>
 #include "Hyperlink.h"
+#include "IDirectionable.h"
 
 using std::string;
 using std::vector;
 
-class TopTenList
+// We are promising to implement the functions in the Interface IDirectionable
+class TopTenList : public IDirectionable
 {
   private:
     vector<Hyperlink> _list;
 
   public:
+    TopTenList();
+
+    // Implement these two functions
+    void display_forward();
+    void display_backward();
     void set_at(int index, Hyperlink link);
     Hyperlink get(int index);
 };
